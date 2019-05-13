97fc8f95d1abcf4ebd50e28d9d27e519109b6a74
Jon Bulica <jonbulica99@gmail.com>
2019-05-13 13:50:16 +0200

    fixes #1 implemented querying port state added parameter for detailed report (--verbose) comented out debug messages (better piping support) update documentation

--- README.md
+++ README.md
@@ -11,9 +11,9 @@ The only thing it does is log into the web management and do the same you would 
 
 This program was written for python3. You are welcome to rewrite it for python2 and make a PR.
 
-The only dependency is `requests`. You can install it using pip:
+The only requirements are `requests` and `BeautifulSoup4` (for HTML parsing). You can install them using pip:
 ```bash
-python3 -m pip install requests
+python3 -m pip install -r requirements.txt
 ```
 Furthermore the script was tested on firmware version `V2.40(AAZI.1)_20180705` of my `GS1900-10HP` model rev `A1`. There are absolutely no guarantees that it will work with your system.
 
@@ -21,7 +21,7 @@ Furthermore the script was tested on firmware version `V2.40(AAZI.1)_20180705` o
 ## Usage
 ```
 usage: poe-manager.py [-h] --host HOST --user USER --password PWD --port PORT
-                      --state {0,1}
+                      [--state {0,1}] [--verbose]
 
 Manage PoE ports for a Zyxel GS1900-10HP switch.
 
@@ -31,9 +31,13 @@ optional arguments:
   --user USER, -U USER  An administrative user.
   --password PWD, -P PWD
                         Password of the admin user.
-  --port PORT, -p PORT  The port number.
+  --port PORT, -p PORT  The port number. When querying information, 0 means
+                        all ports.
   --state {0,1}, -s {0,1}
-                        Turn the port on (1) or off (0).
+                        Turn the port on (1) or off (0). To query the state,
+                        rather than set it, omit this parameter.
+  --verbose, -V         Return detailed information when querying the
+                        specified port state.
 ```
 
 ## Examples
@@ -48,6 +52,25 @@ python3 poe-manager.py --host '10.10.10.2' --user admin --password hunter2 --por
 
 ```bash
 python3 poe-manager.py --host '10.10.10.2' --user admin --password hunter2 --port 3 --state 0
+```
+
+### Get the current state of PoE port 4
+
+```bash
+python3 poe-manager.py --host '10.10.10.2' --user admin --password hunter2 --port 4
+# Output: Enable
+```
+
+Alternatively, use the `--verbose` flag to get even more information for port 4:
+```bash
+python3 poe-manager.py --host '10.10.10.2' --user admin --password hunter2 --port 4 --verbose
+# Output: {'Port': '4', 'State': 'Enable', 'Class': 'class4', 'PD Priority': 'High', 'Power-Up': '802.3at', 'Wide Range Detection': 'Disable', 'Consuming Power (mW)': '3300', 'Max Power (mW)': '31200'}
+```
+
+### Get detailed information for all PoE ports
+
+```bash
+python3 poe-manager.py --host '10.10.10.2' --user admin --password hunter2 --port 0 --verbose
 ```
 
 ### Bash convenience function
--- poe-manager.py
+++ poe-manager.py
@@ -5,13 +5,16 @@ import argparse
 import requests
 from random import random
 from time import sleep, time
+from bs4 import BeautifulSoup
 
 
 def main(args):
-    print("Args:", args)
+    # print("Args:", args)
 
     s = requests.Session()
+    # unfortunately, the ssl ciphers used by zyxel are very old and deprecated, thus plaintext HTTP
     url = "http://{}/cgi-bin/dispatcher.cgi".format(args.host)
+    s.verify = False
 
     login_data = {
         "login": 1,
@@ -21,39 +24,72 @@ def main(args):
     }
     login_check_data = {
         "login_chk": 1,
-        "dummy": current_time( )
+        "dummy": current_time()
     }
 
-    print("Logging in...")
+    # print("Logging in...")
     s.get(url, params=login_data)
-    sleep(1) # implicitely wait for login to occur
+    sleep(1)  # implicitely wait for login to occur
     ret2 = s.get(url, params=login_check_data)
     if not 'OK' in ret2.text:
         raise Exception("Login failed: %s" % ret2.text)
 
-    print("Login successful, parsing cookie.")
+    # print("Login successful, parsing cookie.")
     cookie = parse_cookie(s.get(url, params={"cmd": 1}))
-    print("Got COOKIE: %s" % cookie)
+    # print("Got COOKIE: %s" % cookie)
     s.cookies.set("XSSID", cookie)
 
-    print("Executing command: Turn %s PoE Port %s." % ('on' if args.state else 'off', args.port))
-    command_data = {
-        "XSSID": cookie,
-        "portlist": args.port,
-        "state": args.state,
-        "portPriority": 2,
-        "portPowerMode": 3,
-        "portRangeDetection": 0,
-        "portLimitMode": 0,
-        "poeTimeRange": 20,
-        "cmd": 775,
-        "sysSubmit": "Apply"
-    }
-    ret = s.post(url, data=command_data)
-    if 'window.location.replace' in ret.text:
-        print("Command executed successfully!")
+    if args.state is None:
+        ret = s.get(url, params={"cmd": 773})
+        if ret.ok:
+            soup = BeautifulSoup(ret.content, 'html.parser')
+            table = soup.select("table")[2]
+            data = []
+            # https://stackoverflow.com/a/23377804
+            for row in table.find_all('tr'):
+                cols = row.find_all('td')
+                cols = [ele.text.strip() for ele in cols]
+                data.append([ele for ele in cols if ele])
+
+            ret = []
+            for entry in data[3:]:
+                if entry:
+                    entr = {}
+                    for i, item in enumerate(entry[:-1]):
+                        entr[data[0][i]] = item
+                    ret.append(entr)
+            output = None
+            if args.port:
+                if args.verbose:
+                    output = ret[args.port-1]
+                else:
+                    output = ret[args.port-1].get("State")
+            else:
+                output = ret
+            print(output)
+        else:
+            raise Exception("Failed to fetch the state of PoE port %s. Got response: %s" % (
+                args.port, ret.text))
     else:
-        raise Exception("Failed to execute command: %s" % ret.text)
+        print("Executing command: Turn %s PoE Port %s." %
+              ('on' if args.state else 'off', args.port))
+        command_data = {
+            "XSSID": cookie,
+            "portlist": args.port,
+            "state": args.state,
+            "portPriority": 2,
+            "portPowerMode": 3,
+            "portRangeDetection": 0,
+            "portLimitMode": 0,
+            "poeTimeRange": 20,
+            "cmd": 775,
+            "sysSubmit": "Apply"
+        }
+        ret = s.post(url, data=command_data)
+        if 'window.location.replace' in ret.text:
+            print("Command executed successfully!")
+        else:
+            raise Exception("Failed to execute command: %s" % ret.text)
 
 
 def current_time():
@@ -72,14 +108,14 @@ def encode(_input):
             _len -= 1
             password += _input[_len]
         elif (i == 123):
-          if (lenn < 10):
-            password += "0"
-          else:
-            password += str(math.floor(lenn/10))
+            if (lenn < 10):
+                password += "0"
+            else:
+                password += str(math.floor(lenn/10))
         elif (i == 289):
-          password += str(lenn % 10)
+            password += str(lenn % 10)
         else:
-          password += possible[math.floor(random() * len(possible))]
+            password += possible[math.floor(random() * len(possible))]
         i += 1
     return password
 
@@ -91,10 +127,17 @@ def parse_cookie(cmd_1):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Manage PoE ports for a Zyxel GS1900-10HP switch.')
-    parser.add_argument('--host', '-H', dest='host', required=True, help='The hostname of the switch.')
-    parser.add_argument('--user', '-U', dest='user', required=True, help='An administrative user.')
-    parser.add_argument('--password', '-P', dest='pwd', required=True, help='Password of the admin user.')
-    parser.add_argument('--port', '-p', dest='port', type=int, required=True, help='The port number.')
-    parser.add_argument('--state', '-s', dest='state', type=int, choices=[0,1], required=True, help='Turn the port on (1) or off (0).')
+    parser = argparse.ArgumentParser(
+        description='Manage PoE ports for a Zyxel GS1900-10HP switch.')
+    parser.add_argument('--host', '-H', dest='host',
+                        required=True, help='The hostname of the switch.')
+    parser.add_argument('--user', '-U', dest='user',
+                        required=True, help='An administrative user.')
+    parser.add_argument('--password', '-P', dest='pwd',
+                        required=True, help='Password of the admin user.')
+    parser.add_argument('--port', '-p', dest='port', type=int, required=True,
+                        help='The port number. When querying information, 0 means all ports.')
+    parser.add_argument('--state', '-s', dest='state', type=int, 
+                        choices=[0, 1], help='Turn the port on (1) or off (0). To query the state, rather than set it, omit this parameter.')
+    parser.add_argument('--verbose', '-V', dest='verbose', action="store_true", help='Return detailed information when querying the specified port state.')
     main(parser.parse_args())
